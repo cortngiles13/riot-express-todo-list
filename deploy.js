ba9b3505bcc141ef2aa36fdb54e04052870f26f1
cortngiles13 <cortngiles@gmail.com>
2019-02-14 13:04:35 -0600

    deploy ip address changed

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '35.173.200.223',
+      host: '3.82.235.41',
       username: 'ubuntu',
       privateKey: 'ts-key.pem'
     })
